50f093cc74076105af1e24e2f34d7b6910a39987
Computhink-CS130 <cs130@computhink.com.sg>
2025-01-19 04:08:45 +0000

    Jan 19, 2025, 4:08 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -29,7 +29,7 @@
 #create glass bin variable
 #create plastic bin variable
 #create paper bin variable
-#create general waste bin 
+#create general waste bin variable
 #repeat until items in recycling bin = 0
   #detect material of items
     #if material is glass:
